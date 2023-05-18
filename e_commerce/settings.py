e967220a46aa2f662883b1b0c2ea8b7d7526c805
yurim <yuri.martins@sou.unaerp.edu.br>
2023-05-18 17:17:18 -0300

    insert staticfiles_dir in src/ecommerce/settings.py

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -118,7 +118,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+     os.path.join(BASE_DIR, "static_local")
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
