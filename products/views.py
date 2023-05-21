7d36288a813f4bbdfb2822a1876169149855232e
yurim <yuri.martins@sou.unaerp.edu.br>
2023-05-21 04:00:32 -0300

    import ProductDetailView in src/e_commerce/urls.py, create src/products/templates/detal.html and insert ProductDetailView class in src/products/views.py

--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
-from django.views.generic import ListView
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from .models import Product
@@ -11,16 +11,26 @@ class ProductListView(ListView):
     template_name = "products/list.html"
 
     def get_context_data(self, *args, **kwargs):
-         context = super(ProductListView, self).get_context_data(*args, **kwargs)
-         print(context)
-         return context
+        context = super(ProductListView, self).get_context_data(*args, **kwargs)
+        print(context)
+        return context
 
 
     
 #Function Based View
-def product_list_view(request):
-     queryset = Product.objects.all()
-     context = {
-         'object_list': queryset
-     }
-     return render(request, "products/list.html", context)
+# def product_list_view(request):
+#      queryset = Product.objects.all()
+#      context = {
+#          'object_list': queryset
+#      }
+#      return render(request, "products/list.html", context)
+
+class ProductDetailView(DetailView):
+    #traz todos os produtos do banco de dados sem filtrar nada 
+    queryset = Product.objects.all()
+    template_name = "products/detail.html"
+    
+    def get_context_data(self, *args, **kwargs):
+        context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
+        print(context)
+        return context
